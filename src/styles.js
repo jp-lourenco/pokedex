854e69e8bcc3b67b87650e345423c6fae732adb5
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-11-21 18:50:20 +0000

    initial styles

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 1000px;
+    height: 1000px;
+    background-image: radial-gradient(#444 2%, transparent 6%);
+    background-color: #141414;
+    background-position: 0 0;
+    background-size: 50px 50px;
+`;
