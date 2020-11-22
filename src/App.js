daa390a641643bf99ff30e5d0f8484e8578bbe3b
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-11-22 21:56:50 +0000

    Implemented screen with json and other things

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import Theme from './Theme';
 import {
     Container,
@@ -7,19 +8,63 @@ import {
     Line,
     OtherLine,
     Image,
+    Plus,
+    Minus,
+    StatsContainer,
+    NameContainer,
+    Name,
+    Type,
+    Num,
 } from './styles';
+import pokemonData from './pokemon';
 
 function App() {
+    const [number, setNumber] = useState(
+        Math.floor(Math.random() * pokemonData.pokemon.length),
+    );
+
+    function plusNumber() {
+        if (number < 150) {
+            setNumber(number + 1);
+        }
+    }
+
+    function minusNumber() {
+        if (number > 0) {
+            setNumber(number - 1);
+        }
+    }
+
     return (
         <Theme>
             <Container>
+                <StatsContainer>
+                    <NameContainer>
+                        <Name>{pokemonData.pokemon[number].name}</Name>
+                        <Type>
+                            Type:
+                            {pokemonData.pokemon[
+                                number
+                            ].type.map((item, index) =>
+                                index === 0 ? ` ${item}` : ` - ${item}`,
+                            )}
+                        </Type>
+                        <Num>Number: {pokemonData.pokemon[number].num}</Num>
+                    </NameContainer>
+                </StatsContainer>
                 <PokemonContainer>
+                    <Minus onClick={() => minusNumber()}>
+                        <i className="fa fa-minus" />
+                    </Minus>
                     <Circle>
                         <CircleInside />
                         <Line />
                         <OtherLine />
-                        <Image src="https://i.ibb.co/DgdM9rQ/pokemon-PNG127.png" />
+                        <Image src={pokemonData.pokemon[number].img} />
                     </Circle>
+                    <Plus onClick={() => plusNumber()}>
+                        <i className="fa fa-plus" />
+                    </Plus>
                 </PokemonContainer>
             </Container>
         </Theme>
--- src/styles.js
+++ src/styles.js
@@ -1,13 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    background-image: radial-gradient(#444 2%, transparent 6%);
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    background-image: radial-gradient(#333 1%, transparent 6%);
     background-color: #141414;
     background-position: 0 0;
     background-size: 50px 50px;
 `;
 
 export const PokemonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
     height: 100%;
     padding: 150px 0;
 `;
@@ -21,30 +27,99 @@ export const Circle = styled.div`
 `;
 
 export const CircleInside = styled(Circle)`
-    top: 12%;
-    width: 250px;
-    height: 250px;
+    top: 6%;
+    width: 300px;
+    height: 300px;
     position: relative;
     border: 5px solid ${(props) => props.theme.colors.grass};
 `;
 
 export const Line = styled.div`
-    width: 360px;
+    width: 420px;
     height: 5px;
+    border-radius: 10px;
     background-color: ${(props) => props.theme.colors.grass};
     transform: rotate(-45deg);
     position: relative;
-    top: -20%;
+    top: -50%;
+    right: 10%;
 `;
 
 export const OtherLine = styled(Line)`
-    top: -40%;
+    top: -30%;
 `;
 
 export const Image = styled.img`
-    top: -60%;
-    left: 20%;
-    width: 60%;
-    height: 60%;
+    top: -77%;
+    left: 15%;
+    width: 70%;
+    height: 70%;
     position: relative;
+`;
+
+export const Plus = styled.button`
+    width: 50px;
+    height: 50px;
+    background-color: transparent;
+    border: none;
+    color: ${(props) => props.theme.colors.grass};
+    font-size: 50px;
+    text-align: center;
+    outline: none;
+    margin-left: 30px;
+
+    &:hover {
+        color: green;
+        cursor: pointer;
+    }
+`;
+
+export const Minus = styled(Plus)`
+    margin-left: 0;
+    margin-right: 30px;
+`;
+
+export const StatsContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 150px 0;
+`;
+
+export const NameContainer = styled.div`
+    width: 280px;
+    height: 100px;
+    background-color: rgba(180, 180, 180, 0.05);
+    padding: 10px 15px;
+
+    &:before {
+        display: block;
+        content: '';
+        width: 50px;
+        height: 5px;
+        background-color: ${(props) => props.theme.colors.grass};
+        margin-top: -13px;
+        margin-bottom: 13px;
+    }
+`;
+
+export const Name = styled.h1`
+    font-size: 18px;
+    font-weight: normal;
+    color: #fff;
+    text-transform: uppercase;
+    padding: 0;
+    margin: 0;
+`;
+
+export const Type = styled.p`
+    font-size: 13px;
+    font-weight: bold;
+    color: #fff;
+    text-transform: uppercase;
+    padding: 0;
+    margin: 18px 0 0 0;
+`;
+
+export const Num = styled(Type)`
+    margin: 2px 0;
 `;
