d9e6b9bfdbfcb3c02207cd4000d9d26702ba30fb
jp-lourenco <59509134+jp-lourenco@users.noreply.github.com>
2020-11-23 12:04:40 +0000

    Theme based on the type of pokemon

--- src/Theme.js
+++ src/Theme.js
@@ -1,22 +1,101 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
+function Theme() {}
 
-const theme = {
+Theme.Grass = {
     colors: {
-        grass: '#32fc4e',
-        fire: '#f57a30',
-        water: '#0200F3',
-    },
-    fonts: ['sans-serif', 'Roboto'],
-    fontSizes: {
-        small: '1em',
-        medium: '2em',
-        large: '3em',
+        primary: '#32fc4e',
+        secundary: 'rgba(0, 255, 0, 0.3)',
     },
 };
 
-const Theme = ({ children }) => (
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
-);
+Theme.Fire = {
+    colors: {
+        primary: '#f57a30',
+        secundary: 'rgba(245, 122, 48, 0.5)',
+    },
+};
+
+Theme.Water = {
+    colors: {
+        primary: '#0200F3',
+        secundary: 'rgba(0, 0, 255, 0.5)',
+    },
+};
+
+Theme.Dragon = {
+    colors: {
+        primary: '#A890F0',
+        secundary: 'rgba(168, 144, 240, 0.5)',
+    },
+};
+
+Theme.Psychic = {
+    colors: {
+        primary: 'rgb(255, 0, 127)',
+        secundary: 'rgba(255, 0, 127, 0.5)',
+    },
+};
+
+Theme.Rock = {
+    colors: {
+        primary: 'rgb(155, 75, 0)',
+        secundary: 'rgba(155, 75, 0, 0.5)',
+    },
+};
+
+Theme.Normal = {
+    colors: {
+        primary: 'rgb(155, 155, 150)',
+        secundary: 'rgba(150, 155, 150, 0.5)',
+    },
+};
+
+Theme.Electric = {
+    colors: {
+        primary: 'rgb(255, 255, 0)',
+        secundary: 'rgba(255, 255, 0, 0.5)',
+    },
+};
+
+Theme.Ice = {
+    colors: {
+        primary: 'rgb(165, 242, 243)',
+        secundary: 'rgba(165, 242, 243, 0.5)',
+    },
+};
+
+Theme.Bug = {
+    colors: {
+        primary: 'rgb(168, 184, 32)',
+        secundary: 'rgba(168, 184, 32, 0.5)',
+    },
+};
+
+Theme.Ground = {
+    colors: {
+        primary: 'rgb(224, 192, 104)',
+        secundary: 'rgba(224, 192, 104, 0.5)',
+    },
+};
+
+Theme.Poison = {
+    colors: {
+        primary: 'rgb(160, 64, 160)',
+        secundary: 'rgba(160, 64, 160, 0.5)',
+    },
+};
+
+Theme.Fighting = {
+    colors: {
+        primary: 'rgb(192, 48, 40)',
+        secundary: 'rgba(192, 48, 40, 0.5)',
+    },
+};
+
+Theme.Ghost = {
+    colors: {
+        primary: 'rgb(112, 88, 152)',
+        secundary: 'rgba(112, 88, 152, 0.5)',
+    },
+};
 
 export default Theme;
